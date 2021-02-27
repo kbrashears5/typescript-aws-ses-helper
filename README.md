bb882ee1f20595ec58da95ebd4ff134ed3dfdbab
Kevin Brashears <kbrashears5@gmail.com>
2021-02-26 21:49:02 -0600

    converting to eslint

--- README.md
+++ README.md
@@ -10,46 +10,55 @@
 
 [![NPM Version](https://img.shields.io/npm/v/typescript-aws-ses-helper)](https://img.shields.io/npm/v/typescript-aws-ses-helper)
 [![Downloads](https://img.shields.io/npm/dt/typescript-aws-ses-helper)](https://img.shields.io/npm/dt/typescript-aws-ses-helper)
+
 </div>
 
 ## Install
+
 ```
 npm install typescript-aws-ses-helper@latest
 ```
 
 ## Usage
+
 ### Default - running in Lambda in your own account
+
 ```typescript
 const logger = new Logger(LogLevel.Trace);
 
 const helper = new SESHelper(logger);
 
-const response = await helper.SendEmailAsync('subject',
-    ['toAddresses'],
-    'fromAddress',
-    'body');
+const response = await helper.SendEmailAsync(
+  'subject',
+  ['toAddresses'],
+  'fromAddress',
+  'body',
+);
 ```
 
 ### Running in separate account or not in Lambda
+
 ```typescript
 const logger = new Logger(LogLevel.Trace);
 
 const options: AWS.SES.ClientConfiguration = {
-    accessKeyId: '{access_key}',
-    secretAccessKey: '{secret_key}',
-    region: 'us-east-1',
+  accessKeyId: '{access_key}',
+  secretAccessKey: '{secret_key}',
+  region: 'us-east-1',
 };
 
 const repository = new AWS.SES(options);
 
-const helper = new SESHelper(logger,
-    repository);
+const helper = new SESHelper(logger, repository);
 
-const response = await helper.SendEmailAsync('subject',
-    ['toAddresses'],
-    'fromAddress',
-    'body');
+const response = await helper.SendEmailAsync(
+  'subject',
+  ['toAddresses'],
+  'fromAddress',
+  'body',
+);
 ```
 
 ## Notes
+
 If no options are supplied, will default to `us-east-1` as the region
--- src/test-values.ts
+++ src/test-values.ts
@@ -4,26 +4,28 @@ import * as SES from '@aws-sdk/client-ses';
  * Test values
  */
 export class TestingValues {
-    // descriptions
-    public AWSError: string = 'AWS Error';
-    public InvalidTest: string = 'returns error from AWS';
-    public MustSupply: string = 'Must supply';
-    public ThrowsOnEmpty: string = 'throws on empty';
-    public ValidTest: string = 'returns valid response from AWS';
+  // descriptions
+  public AWSError: string = 'AWS Error';
+  public InvalidTest: string = 'returns error from AWS';
+  public MustSupply: string = 'Must supply';
+  public ThrowsOnEmpty: string = 'throws on empty';
+  public ValidTest: string = 'returns valid response from AWS';
 
-    // empty values
-    public EmptyArray = [];
-    public EmptyString: string = '';
+  // empty values
+  public EmptyArray = [];
+  public EmptyString: string = '';
 
-    // strings
-    public Body: string = 'body';
-    public ContentType: string = 'content-type';
-    public EmailAddress: string = 'email-address';
-    public MessageId: string = 'message-id';
-    public Name: string = 'name';
-    public Subject: string = 'string';
+  // strings
+  public Body: string = 'body';
+  public ContentType: string = 'content-type';
+  public EmailAddress: string = 'email-address';
+  public MessageId: string = 'message-id';
+  public Name: string = 'name';
+  public Subject: string = 'string';
 
-    // objects
-    public SendEmailResponse: SES.SendEmailResponse = { MessageId: 'message-id' };
-    public SendRawEmailResponse: SES.SendRawEmailResponse = { MessageId: 'message-id' }
+  // objects
+  public SendEmailResponse: SES.SendEmailResponse = { MessageId: 'message-id' };
+  public SendRawEmailResponse: SES.SendRawEmailResponse = {
+    MessageId: 'message-id',
+  };
 }
